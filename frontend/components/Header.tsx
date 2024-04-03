6f3039247af81e23d4a77e65ea7c64b2481429d6
Naosuke Ito <Naosuke.Ito@ibm.com>
2024-04-03 16:16:05 +0900

    add image

---
+++ frontend/components/Header.tsx
@@ -0,0 +1,5 @@
+const Header: React.FC = () => {
+  return <header className="text-3xl font-bold p-4">Title</header>;
+};
+
+export default Header;
---
+++ frontend/components/ImageBox.tsx
@@ -0,0 +1,5 @@
+const CommentBox: React.FC = () => {
+  return <div className="border p-6">Comment</div>;
+};
+
+export default CommentBox;
